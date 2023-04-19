140e2bde6ef55cd8bc19de15606ee2f5864eb205
MaiCodes-exe <maise.b.costa@gmail.com>
2023-04-19 20:50:58 +0000

    added the func to delete non alphabetic characters

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 function palindrome(str) {
+  for (let i = 0; i < str.length; i++){
+ if (str[i] < 'A' || str[i] > 'Z' &&
+                    str[i] < 'a' || str[i] > 'z')
+            {
+                   
+                // erase function to erase
+                // the character
+                str = str.substring(0, i) + str.substring(i + 1);
+                i--;
+            }
+  }
   return true;
 }
 
